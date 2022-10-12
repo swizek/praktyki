5e607b5688e71a883ea2ac76a5ada4aa56c888d1
swizek <81292369+swizek@users.noreply.github.com>
2022-10-12 12:10:00 +0200

    zadanie

---
+++ modyfikatorout/modyfikatorout/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace modyfikatorout
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random random = new Random();
+        public int ReturnThreeValues(out double half, out int twice)
+        {
+            int value = random.Next(1000);
+            half = ((double)value) / 2;
+            twice = value * 2;
+            return value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a;
+            double b;
+            int c;
+            a = ReturnThreeValues(out b, out c);
+            Console.WriteLine("value = {0}, half = {1}, double = {2}", a, b, c);
+        }
+        public void ModifyAnIntAndButton(ref int value, ref Button button)
+        {
+            int i = value;
+            i *= 5;
+            value = i - 3;
+            button = button1;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int q = 100;
+            Button b = button2;
+            ModifyAnIntAndButton(ref q, ref b);
+            Console.WriteLine("q = {0}, b.Text = {1}, q, b.Text");
+        }
+    }
+}
---
+++ zadaniedog/zadaniedog/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace zadaniedog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public struct Dog
+        {
+            public string Name;
+            public string Breed;
+            public Dog(string name, string breed)
+            {
+                this.Name = name;
+                this.Breed = breed;
+
+            }
+            public void Speak()
+            {
+                Console.WriteLine("Wabie się {0}. Moja rasa to {1}", Name, Breed);
+            }
+            public class Canine
+            {
+                public string Name;
+                public string Breed;
+                private int mealSize;
+
+                public double MealSize { get; private set; }
+
+                public Canine(string name, string breed)
+                {
+                    this.Name = name;
+                    this.Breed = breed;
+                }
+                public void WalkDogOrCanine(object getsWalked)
+                {
+                    Dog sid = new Dog("Sid", "husky");
+                    WalkDogOrCanine(sid);
+                    Dog happy = (Dog)getsWalked;
+                }
+                public double FeedDog(Canine dogToFeed, Bowl dogBowl)
+                {
+                    double eaten = Eat(dogToFeed.MealSize, dogBowl);
+                    return eaten + .05D;
+
+
+
+                }
+                public void Eat(double mealSize, Bowl dogBowl)
+                {
+                    dogBowl.Capacity -= mealSize;
+                    CheckBowl(dogBowl.Capacity);
+                }
+            }
+                public void CheckBowl(double capacity)
+                {
+                    if (capacity < 12.5D)
+                { 
+                        string message = "Moja miska jest prawie pusta!";
+                        Console.WriteLine(message);
+
+                    }
+                }
+            }
+        }
+
+    public class Bowl
+    {
+        public double Capacity { get; internal set; }
+    }
+}
+
